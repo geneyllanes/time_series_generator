72ac7ece1e2f3a9d61d2a371a77c51e0f9f96c14
geneyllanes <gene.yllanes@americansystems.com>
2023-05-17 18:16:53 -0400

    optimizations
    Better separation of concerns
    performance enhancement in the loops

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,5 @@
 import 'package:time_series_generator/src/server.dart';
 
 Future<void> main(List<String> args) async {
-  await Server().main(args);
+  await Server.main(args);
 }
--- lib/src/server.dart
+++ lib/src/server.dart
@@ -1,14 +1,24 @@
 import 'dart:async';
-import 'package:grpc/grpc.dart' as grpc;
 import 'dart:math';
+import 'package:grpc/grpc.dart' as grpc;
 import 'package:logging/logging.dart';
 import 'package:time_series_generator/src/generated/time_series_generator.dart';
 
 final Logger _logger = Logger('TimeSeriesGenerator');
 
 class TimeSeriesGeneratorService extends TimeSeriesGeneratorServiceBase {
-  StreamController<TimeSeriesData> controller =
-      StreamController<TimeSeriesData>();
+  late final StreamController<TimeSeriesData> _controller;
+  bool _hasActiveSubscribers = false;
+
+  TimeSeriesGeneratorService() {
+    _controller = StreamController<TimeSeriesData>(
+      onCancel: () {
+        _logger.info('Stream canceled. Stopping time series generation.');
+        _hasActiveSubscribers = false;
+        _controller.close();
+      },
+    );
+  }
 
   @override
   Stream<TimeSeriesData> generateTimeSeries(
@@ -16,77 +26,82 @@ class TimeSeriesGeneratorService extends TimeSeriesGeneratorServiceBase {
     final sampleRate = request.sampleRate;
     final toneConfigs = request.tones;
 
-    generateTimeSeriesFromConfig(sampleRate, toneConfigs, controller);
+    _hasActiveSubscribers = true;
+    generateTimeSeriesFromConfig(sampleRate, toneConfigs);
 
-    return controller.stream;
+    return _controller.stream;
   }
 
   void generateTimeSeriesFromConfig(
-      double sampleRate,
-      List<ToneConfig> toneConfigs,
-      StreamController<TimeSeriesData> controller) {
-    // Generates a time series with sine waves for each tone
-    // and streams the data points to the client based on the provided
-    // configuration
-    // This implementation generates time series indefinitely
+      double sampleRate, List<ToneConfig> toneConfigs) {
+    _controller.addStream(
+      generateTimeSeriesStream(sampleRate, toneConfigs),
+    );
+  }
 
-    final completer = Completer<void>();
+  /// In this optimization:
+  /// Instead of repeatedly accessing the properties of
+  /// ToneConfig objects inside the inner loop,
+  /// the values of frequency, amplitude, and initialPhase
+  /// are pre-extracted into separate lists (constants, amplitudes,
+  /// and initialPhases) outside the loop.
+  ///
+  /// The length of toneConfigs is stored in toneConfigsLength
+  /// variable to avoid redundant calculations.
+  ///
+  /// The lists constants, amplitudes, and initialPhases
+  /// are pre-allocated with the required length to avoid
+  /// unnecessary allocations within the loop.
+  ///
+  Stream<TimeSeriesData> generateTimeSeriesStream(
+      double sampleRate, List<ToneConfig> toneConfigs) async* {
+    _logger.info('Starting time series generation.');
 
-    // Generate time series until completion is signaled
-    Timer.periodic(Duration(milliseconds: (1000 / sampleRate).round()),
-        (timer) {
-      // checking if the stream has any active listeners
-      // if not then cancel and complete
-      if (!controller.hasListener) {
-        timer.cancel();
-        completer.complete();
-        return;
-      }
+    final timeSeries = List<double>.filled(1000, 0.0);
+    final int toneConfigsLength = toneConfigs.length;
 
-      final timeSeries = <double>[];
+    final constants = List<double>.filled(toneConfigsLength, 0.0);
+    final amplitudes = List<double>.filled(toneConfigsLength, 0.0);
+    final initialPhases = List<double>.filled(toneConfigsLength, 0.0);
 
-      // Generate the time series for the current tone
+    for (var i = 0; i < toneConfigsLength; i++) {
+      final toneConfig = toneConfigs[i];
+      constants[i] = 2 * pi * toneConfig.frequency / sampleRate;
+      amplitudes[i] = toneConfig.amplitude;
+      initialPhases[i] = toneConfig.initialPhase;
+    }
+
+    while (_hasActiveSubscribers) {
       for (var t = 0; t < 1000; t++) {
         double value = 0.0;
-        for (final toneConfig in toneConfigs) {
-          final initialPhase = toneConfig.initialPhase;
-          final amplitude = toneConfig.amplitude;
-          final frequency = toneConfig.frequency;
 
-          // Generate the value for the current time point
-          value += amplitude *
-              sin(2 * pi * frequency * t / sampleRate + initialPhase);
+        for (var i = 0; i < toneConfigsLength; i++) {
+          value += amplitudes[i] * sin(constants[i] * t + initialPhases[i]);
         }
-        timeSeries.add(value);
+
+        timeSeries[t] = value;
       }
 
-      // Create a response containing the generated time series
-      final response = TimeSeriesData()..data.addAll(timeSeries);
+      yield TimeSeriesData()..data.addAll(timeSeries);
 
-      // Yield the response to the client through the stream controller
-      controller.add(response);
-
-      // Log the generated time series
-      _logger.fine('Generated time series: $timeSeries');
-    });
-
-    // When completion is signaled, close the stream
-    completer.future.then((_) {
-      controller.close();
-      _logger.info('Time series generation completed.');
-    });
+      await Future.delayed(Duration(milliseconds: (1000 / sampleRate).round()));
+    }
   }
 }
 
 class Server {
-  Future<void> main(List<String> args) async {
-    // Configure the logger
+  static void configureLogger() {
     Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen((record) {
       print('${record.level.name}: ${record.time}: ${record.message}');
     });
+  }
+
+  static Future<void> main(List<String> args) async {
+    configureLogger();
 
     final server = grpc.Server([TimeSeriesGeneratorService()]);
+
     await server.serve(port: 8080);
     _logger.info('Server listening on port ${server.port}...');
   }
